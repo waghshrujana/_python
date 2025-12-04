32af44a16ac3a7573fafb14d304c81e503675da0
waghshrujana <shrujanawagh@gmail.com>
2025-12-04 14:31:41 +0530

    Add files via upload

---
+++ arithmeticoperations.py
@@ -0,0 +1,12 @@
+num1 = float(input("Enter the first number: "))
+num2 = float(input("Enter the second number: "))
+addition = num1 + num2
+subtraction = num1 - num2
+multiplication = num1 * num2
+division = num1 / num2
+print(f"\n Results of Arithmetic Operations:")
+print(f"{num1} + {num2} = {addition}")
+print(f"{num1} - {num2} = {subtraction}")
+print(f"{num1} * {num2} = {multiplication}")
+print(f"{num1} / {num2} = {division}")
+
