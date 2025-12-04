569ad8c6918ac3a383695a9e205fabe048a2239a
waghshrujana <shrujanawagh@gmail.com>
2025-12-04 14:43:26 +0530

    Add files via upload

---
+++ list_element_accessing_in_negative_indexing.py
@@ -0,0 +1,5 @@
+my_list = ["apple", "banana", "cherry", "date"]
+last_element = my_list[-1]  # Accesses "date"
+second_to_last = my_list[-2] # Accesses "cherry"
+print(last_element)
+print(second_to_last)
