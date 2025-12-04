8e7fc40958babd83b3408142612c9c18edf42ffa
waghshrujana <shrujanawagh@gmail.com>
2025-12-04 14:41:38 +0530

    Add files via upload

---
+++ list_element_accessing_in_positive_indexing.py
@@ -0,0 +1,5 @@
+my_list = ["apple", "banana", "cherry", "date"]
+first_element = my_list[0]  # Accesses "apple"
+third_element = my_list[2]  # Accesses "cherry"
+print(first_element)
+print(third_element)
