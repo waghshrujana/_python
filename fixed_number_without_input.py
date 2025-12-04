799988ffc2d4a1e1fde16ac141df4dabbc3729df
waghshrujana <shrujanawagh@gmail.com>
2025-12-04 15:28:03 +0530

    Add files via upload

---
+++ fixed_number_without_input.py
@@ -0,0 +1,6 @@
+fixed_number = 15
+if fixed_number & 1:  # If the result of bitwise AND with 1 is non-zero (i.e., 1)
+    print(f"{fixed_number} is an odd number.")
+else:
+    print(f"{fixed_number} is an even number.")
+
