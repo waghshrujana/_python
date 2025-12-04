2d66d9c012dc08f741c453db4bfe7c4de84b4cdb
waghshrujana <shrujanawagh@gmail.com>
2025-12-04 14:46:14 +0530

    Add files via upload

---
+++ list_slicing.py
@@ -0,0 +1,9 @@
+my_list = ["apple", "banana", "cherry", "date", "elderberry", "fig"]
+sub_list1 = my_list[1:4] 
+sub_list2 = my_list[:3]  
+sub_list3 = my_list[2:]   
+sub_list4 = my_list[::2] 
+print(sub_list1)
+print(sub_list2)
+print(sub_list3)
+print(sub_list4)
